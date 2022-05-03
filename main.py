d044bc84d1d6773933ea32f352a562ee9125df03
Mxchaeltrxn <tranmichael97@gmail.com>
2022-05-04 07:52:06 +1000

    fix: reverse insertion order in program to make printing more obvious

--- main.py
+++ main.py
@@ -8,15 +8,17 @@ def printReverse(node):
     return
 
 linked_list = SinglyLinkedList()
-linked_list.insert(0, 1)
-linked_list.insert(0, 2)
-linked_list.insert(0, 3)
-linked_list.insert(0, 4)
-linked_list.insert(0, 5)
-linked_list.insert(0, 6)
-linked_list.insert(0, 7)
-linked_list.insert(0, 8)
-linked_list.insert(0, 9)
-linked_list.insert(0, 10)
 
+linked_list.insert(0, 10)
+linked_list.insert(0, 9)
+linked_list.insert(0, 8)
+linked_list.insert(0, 7)
+linked_list.insert(0, 6)
+linked_list.insert(0, 5)
+linked_list.insert(0, 4)
+linked_list.insert(0, 3)
+linked_list.insert(0, 2)
+linked_list.insert(0, 1)
+
+# should print 10 9 8 7 6 5 4 3 2 1
 printReverse(linked_list.head)
