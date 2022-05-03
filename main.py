3eab1be1cc15833fd43aa42bbc842fdbb69e096e
Mxchaeltrxn <tranmichael97@gmail.com>
2022-05-03 18:16:21 +1000

    feat: use linked list in program

---
+++ main.py
@@ -0,0 +1,22 @@
+from singly_linked_list import LinkedList
+
+def printReverse(node):
+  if node is not None:
+    printReverse(node.next_node)
+    print(node.data)
+  else:
+    return
+
+linked_list = LinkedList()
+linked_list.insert(0, 1)
+linked_list.insert(0, 2)
+linked_list.insert(0, 3)
+linked_list.insert(0, 4)
+linked_list.insert(0, 5)
+linked_list.insert(0, 6)
+linked_list.insert(0, 7)
+linked_list.insert(0, 8)
+linked_list.insert(0, 9)
+linked_list.insert(0, 10)
+
+printReverse(linked_list.head)
